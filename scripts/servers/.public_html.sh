5fe9dae11f88d047385a490d98d6b19dccc6bba4
dyngar <dyngar-m@dyngar-dev.local>
2014-01-04 17:27:13 -0500

    Single-user local web server is set up to sync with ~/.public_html using rsync. This isn't going to handle virtual hosting, but that's not the idea(with this one). It's just for people to play with webpages. make sure to offer a choice of one or the other, both will break it.

---
+++ scripts/servers/.public_html.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+rsync -razv .public_html/ .OS/lighttpd/var/www/
+
--- scripts/servers/web-serv.sh
+++ scripts/servers/web-serv.sh
@@ -6,4 +6,11 @@ chroot .OS/lighttpd
 apt-get install lighttpd php5-cgi
 exit
 mkdir ~/public_html
+cp ./.public_html.sh home/*/public_html.sh
+chmod +x .public_html.sh
+cp /var/spool/cron/crontabs/root /var/spool/cron/crontabs/root.tmp
+rm /var/spool/cron/crontabs/root
+cat /var/spool/cron/crontabs/root.tmp "*/2 * * * * /home/*/.public_html.sh
+" > /var/spool/cron/crontabs/root
+rm /var/spool/cron/crontabs/root.tmp
 echo "[*] Web server installed."
