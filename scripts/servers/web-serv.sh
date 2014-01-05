be98eff005d3c9654ffa01eeae7ee33d94ae36b2
dkoksal <w4nder1n9w12ard@gmail.com>
2014-01-05 00:38:22 -0500

    set up default lighttpd.conf file in web servers config folder

--- scripts/servers/web-serv.sh
+++ scripts/servers/web-serv.sh
@@ -4,6 +4,12 @@ mkdir -p .OS/lighttpd
 debootstrap wheezy .OS/lighttpd http://http.debian.net/debian
 chroot .OS/lighttpd
 apt-get install lighttpd php5-cgi
+cp /var/spool/cron/crontabs/root /var/spool/cron/crontabs/root.tmp
+rm /var/spool/cron/crontabs/root
+cat /var/spool/cron/crontabs/root.tmp "@reboot service lighttpd restart
+
+" > /var/spool/cron/crontabs/root
+rm /var/spool/cron/crontabs/root.tmp
 exit
 mkdir ~/public_html
 cp ./.public_html.sh home/*/public_html.sh
@@ -11,6 +17,9 @@ chmod +x .public_html.sh
 cp /var/spool/cron/crontabs/root /var/spool/cron/crontabs/root.tmp
 rm /var/spool/cron/crontabs/root
 cat /var/spool/cron/crontabs/root.tmp "*/2 * * * * /home/*/.public_html.sh
+@reboot /home/*/.public_html.sh
+
 " > /var/spool/cron/crontabs/root
 rm /var/spool/cron/crontabs/root.tmp
+cp config/lighttpd.conf .OS/lighttpd/etc/lighttpd.conf
 echo "[*] Web server installed."
