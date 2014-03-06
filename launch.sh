d94241e7ab17ef0edd8791a678753723e7ae0314
dyngar <w4nder1n9w12ard@gmail.com>
2014-03-06 17:50:57 -0500

    enabled all the scripts

--- launch.sh
+++ launch.sh
@@ -7,63 +7,74 @@
 
 source prompt.sh
 source scripts/security/harden-setup.sh
-#source scripts/sysmonitor/desktop-mon.sh
-#source scripts/sysmonitor/term-mon.sh
-#source scripts/security/iptables.sh
-#source scripts/security/browser-setup.sh
-#source scripts/security/tor-suite.sh
-#source scripts/security/ids-setup.sh
-#source scripts/servers/web-serv.sh
-#source scripts/servers/meshnet.sh
-#source scripts/development/game-dev.sh
-#source scripts/development/gnome-dev.sh
-#source scripts/security/baseline-cleanup.sh
-#source scripts/security/tiger-setup.sh
+source scripts/sysmonitor/desktop-mon.sh
+source scripts/sysmonitor/term-mon.sh
+source scripts/security/iptables.sh
+source scripts/security/browser-setup.sh
+source scripts/security/tor-suite.sh
+source scripts/security/ids-setup.sh
+source scripts/servers/web-serv.sh
+source scripts/servers/meshnet.sh
+source scripts/development/game-dev.sh
+source scripts/development/gnome-dev.sh
+source scripts/security/baseline-cleanup.sh
+source scripts/security/tiger-setup.sh
 INITIAL_PROMPT
 echo $t
 if [ t ]
 then
 #	PROMPT 
-	if [ [ PROMPT ${Hardener[1]} ] ]
+	if [ [ PROMPT $Hardener[1] ] ]
 	then
 		INSTALL_HARDEN_KERNEL_PATCHES
 	fi
-#	if [ PROMPT $EmbeddedDesktop[1] ]
-#		INSTALL_EMBEDDED_DESKTOP_INFO
-#	fi
-#	if [ PROMPT $TerminalMonitoring[1] ]
-#		INSTALL_TERMINAL_MONITORING
-#	fi
-#	if [ PROMPT $BrowserSecurity[1] ]
-#		INSTALL_BROWSER_SECURITY_PLUGINS
-#	fi
-#	if [ PROMPT $FireWall[1] ]
-#		INSTALL_FIREWALL_CONFIG_TOOLS
-#	fi
-#	if [ PROMPT $TorSuite[1] ]
-#		INSTALL_TOR_SUITE
-#	fi
-#	if [ PROMPT $IntruderDetect[1] ]
-#		INSTALL_INTRUSION_DETECTION_SYSTEM
-#	fi
-#	if [ PROMPT $WebServer[1] ]
-#		INSTALL_WEB_SERVER
-#	fi
-#	if [ PROMPT $MeshRouter[1] ]
-#		INSTALL_MESH_NETWORKING
-#	fi
-#	if [ PROMPT $Gtk3Development[1] ]
-#		INSTALL_GNOME3_DEVELOPMENT
-#	fi
-#	if [ PROMPT $GameDevelopment[1] ]
-#		INSTALL_GAME_DEVELOPMENT_PACKAGES
-#	fi
-#	if [ PROMPT $FirstCleanup[1] ]
-#		CLEANUP_ALL_UNNECESSARY_APPS
-#	fi
-#	if [ PROMPT $TigerDetection[1] ]
-#		INSTALL_TIGER_SYSTEM_MONITOR
-#	fi
+	if [ PROMPT $EmbeddedDesktop[1] ]
+		INSTALL_EMBEDDED_DESKTOP_INFO
+	fi
+	if [ PROMPT $TerminalMonitoring[1] ]
+	then
+		INSTALL_TERMINAL_MONITORING
+	fi
+	if [ PROMPT $BrowserSecurity[1] ]
+	then
+		INSTALL_BROWSER_SECURITY_PLUGINS
+	fi
+	if [ PROMPT $FireWall[1] ]
+	then
+		INSTALL_FIREWALL_CONFIG_TOOLS
+	fi
+	if [ PROMPT $TorSuite[1] ]
+	then
+		INSTALL_TOR_SUITE
+	fi
+	if [ PROMPT $IntruderDetect[1] ]
+	then
+		INSTALL_INTRUSION_DETECTION_SYSTEM
+	fi
+	if [ PROMPT $WebServer[1] ]
+	then
+		INSTALL_WEB_SERVER
+	fi
+	if [ PROMPT $MeshRouter[1] ]
+	then
+		INSTALL_MESH_NETWORKING
+	fi
+	if [ PROMPT $Gtk3Development[1] ]
+	then
+		INSTALL_GNOME3_DEVELOPMENT
+	fi
+	if [ PROMPT $GameDevelopment[1] ]
+	then
+		INSTALL_GAME_DEVELOPMENT_PACKAGES
+	fi
+	if [ PROMPT $FirstCleanup[1] ]
+	then
+		CLEANUP_ALL_UNNECESSARY_APPS
+	fi
+	if [ PROMPT $TigerDetection[1] ]
+	then
+		INSTALL_TIGER_SYSTEM_MONITOR
+	fi
 fi
 echo "
 \$runonce=1
